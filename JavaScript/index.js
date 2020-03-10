e6aef3d621d471ab563ab6708a83ae3aad2b39ba
aazucena <aldrinp.azucena@gmail.com>
2020-03-10 17:50:26 -0600

    Updated html files, in progress for sell.html

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,12 +1,32 @@
 $(document).ready(function () {
-    $('[data-toggle="popover"]').popover({
+    $("[data-toggle='popover']").popover({
         html: true,
         content: function () {
-            return $('#popover-content').html();
+            return $("#popover-content").html();
         }
     });
+    $(".cfield").hide();
 });
-$('#navList a').on('click', function (e) {
+$("#navList a").on("click", function (e) {
     e.preventDefault();
-    $(this).tab('show');
+    $(this).tab("show");
+});
+
+$("#selectCategory").change(function(){
+    var category = $("#selectCategory").val();
+    $(".cfield").hide();
+    switch (category){
+        case "1":
+            $("#hitmen").show();
+            break;
+        case "2":
+            $("#substances").show();
+            break;
+        case "3":
+            $("#exotics").show();
+            break;
+        case "4":
+            $("#weapons").show();
+            break;
+    }
 });
