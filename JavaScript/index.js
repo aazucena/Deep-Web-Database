a43c3db72ae39d87f03ea18036e2344afd083f9a
Aldrin Azucena <43855657+aazucena@users.noreply.github.com>
2020-03-11 01:46:20 -0600

    finished sell.html and update.html

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -5,28 +5,159 @@ $(document).ready(function () {
             return $("#popover-content").html();
         }
     });
-    $(".cfield").hide();
+    var pathname = window.location.pathname;
+    var category = $("#selectCategory").val();
+    if (category && pathname == "/update.html") {
+        $(".cfield").hide();
+        switch (category) {
+            case "1":
+                $("#hitmen").show();
+                $("#post").show();
+                $(".wfield").hide();
+                break;
+            case "2":
+                $("#substances").show();
+                $("#post").show();
+                $(".wfield").hide();
+                break;
+            case "3":
+                $("#exotics").show();
+                $("#post").show();
+                $(".wfield").hide();
+                break;
+            case "4":
+                $("#weapons").show();
+                break;
+        }
+    } else {
+        $(".cfield").hide();
+        $(".wfield").hide();
+        $("#post").hide();
+    }
 });
 $("#navList a").on("click", function (e) {
     e.preventDefault();
     $(this).tab("show");
 });
 
-$("#selectCategory").change(function(){
+$("#selectCategory").change(function () {
     var category = $("#selectCategory").val();
     $(".cfield").hide();
-    switch (category){
+    $("#post").hide();
+    switch (category) {
         case "1":
             $("#hitmen").show();
+            $("#post").show();
+            $(".wfield").hide();
             break;
         case "2":
             $("#substances").show();
+            $("#post").show();
+            $(".wfield").hide();
             break;
         case "3":
             $("#exotics").show();
+            $("#post").show();
+            $(".wfield").hide();
             break;
         case "4":
             $("#weapons").show();
+            break;
+    }
+});
+/** Adding Product Images */
+$('#prodImgs').change(function () {
+
+    var countFiles = $(this)[0].files.length;
+    var $preview = $('#preview').empty();
+    if (this.files) $.each(this.files, readAndPreview);
+
+    function readAndPreview(i, file) {
+        if (countFiles > 1) {
+            $("#pimg").text(countFiles + " Files");
+        } else {
+            $("#pimg").text("1 File");
+        }
+        if (!/\.(jpe?g|png|gif)$/i.test(file.name)) {
+            return alert(file.name + " is not an image");
+        }
+
+        var reader = new FileReader();
+
+        $(reader).on("load", function () {
+            $preview.append('<div class="col-lg-3 col-md-4 col-6' +
+                ' align-self-center text-white rounded-top bg-logored p-3 mx-2">' +
+                file.name + '<img src="' +
+                this.result +
+                '" class="img-fluid align-middle"/></div>');
+        });
+
+        reader.readAsDataURL(file);
+
+    }
+
+});
+
+$('#addprod').click(function () {
+    var type = $('#protypes').val();
+    var count = 0;
+    if (type != null || type == '') {
+        count++;
+        $('#ptype').append($('<option>', {
+            text: type,
+            value: count
+        }));
+    }
+});
+
+$("#wtypes").change(function () {
+    var type = $("#wtypes").val();
+    $(".wfield").hide();
+    $("#post").hide();
+    switch (type) {
+        case "1":
+            $("#arms").show();
+            $("#armtypes").attr({
+                required: "required"
+            });
+            $("#ammo").show();
+            $("#amtype").attr({
+                required: "required",
+                step: "10"
+            });
+            $("#post").show();
+            break;
+        case "2":
+            $("#exp").show();
+            $("#exptypes").attr({
+                required: "required"
+            });
+            $("#ammo").show();
+            $("#amtype").attr({
+                required: "required"
+            });
+            $("#post").show();
+            break;
+        case "3":
+            $("#melee").show();
+            $("#mtypes").attr({
+                required: "required"
+            });
+            $("#post").show();
+            break;
+        case "4":
+            $("#bcn").show();
+            $("#compound").attr({
+                required: "required"
+            });
+            $("#ammo").show();
+            $("#amtype").attr({
+                required: "required"
+            });
+            $("#post").show();
+            break;
+        case "5":
+            $("#post").show();
             break;
     }
 });
